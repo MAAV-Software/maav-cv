07a6ad4591531e085b3459228685a040c5a0ddc8
jepye-3141 <jepye@umich.edu>
2020-11-07 11:27:25 -0700

    Added new benchmarks

---
+++ meanTest.py
@@ -0,0 +1,19 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Check if the webcam is opened correctly
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+dest = '_________'
+i = 0
+while i < 100:
+    ret, frame = cap.read()
+    trans = cv2.blur(frame)
+    cv2.imwrite(dest, trans)
+
+    i++
+
+cap.release()
+cv2.destroyAllWindows()
